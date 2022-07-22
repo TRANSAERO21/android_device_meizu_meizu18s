33a9b8ae8d381f7838bae6dd1b6482adb063aa42
TRANSAERO21 <martynenkodima21@gmail.com>
2022-07-22 23:35:57 +0300

    m2182: include AOSPA configuration

--- aospa_m2182.mk
+++ aospa_m2182.mk
@@ -7,6 +7,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # m2182 configuration
 $(call inherit-product, device/meizu/m2182/m2182.mk)
 
+# AOSPA configuration
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
 PRODUCT_NAME := aospa_m2182
 PRODUCT_DEVICE := m2182
 PRODUCT_BRAND := Meizu
